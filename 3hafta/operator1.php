1d9340c28c90a8bd06d1d260f0f4bcf1e331007c
Timur Daulbay <tdawlbaev@gmail.com>
2025-03-06 10:31:39 +0300

    3 hafta guncellendi

--- 3hafta/operator1.php
+++ 3hafta/operator1.php
@@ -1,1 +1,23 @@
-¡
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $adi = "Furkan";
+    $soyadi = "Aydin";
+
+    $adi_soyadi = $adi . " " . $soyadi;
+
+    $boyu = 183;
+    $kilosu = 89.5;
+
+    echo $adi_soyadi , "<br>";
+
+    echo "Boyu: " . $boyu . "<br> Kilosu: " . $kilosu;
+    ?>
+</body>
+</html>
